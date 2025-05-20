f7c71e1840e7c8e6b0cff9c03ea412ce9b7fcaca
Joel Tannas <jtannas@gmail.com>
2025-05-20 13:56:41 -0600

    make custom CSS for hit slop on touch devices

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,15 @@ export const Route = createFileRoute("/")({
 
 function Index() {
 	return (
-		<div className="p-2">
+		<div className="p-5">
 			<h3>Welcome Home!</h3>
+			<button
+				type="button"
+				className="bg-blue-500 w-fit p-2 slop-3"
+				onClick={() => console.count("click")}
+			>
+				MyButton
+			</button>
 		</div>
 	);
 }
