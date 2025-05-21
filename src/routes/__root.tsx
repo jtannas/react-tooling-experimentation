a7d7fdd7dda1a3e4cf22289b2e3fb4f1c8e616a1
Joel Tannas <jtannas@gmail.com>
2025-05-21 17:12:02 -0600

    create stripped down example of issue

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,39 +1,28 @@
 import {
 	createRootRoute,
 	Link,
+	linkOptions,
 	Outlet,
-	retainSearchParams,
-	stripSearchParams,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import z from "zod";
 
-const rootSearchSchema = z.object({
-	root: z.enum(["a", "b", "c"]).catch("a"),
+const aboutLinkOptions = linkOptions({
+	to: "/about",
+	className: "[&.active]:font-bold",
+	search: { allowed: "foo", notAllowed: "bar" },
 });
 
 export const Route = createRootRoute({
 	component: () => (
 		<>
 			<div className="p-2 flex gap-2">
-				<Link to="/" className="[&.active]:font-bold">
-					Home
-				</Link>{" "}
+				<Link {...aboutLinkOptions}>About</Link>
 				<Link
 					to="/about"
 					className="[&.active]:font-bold"
-					search={{ example: "bar" }}
+					search={{ allowed: "foo", notAllowed: "bar" }}
 				>
 					About
-				</Link>
-				<Link to="/sample" className="[&.active]:font-bold">
-					Sample
-				</Link>
-				<Link to="/settings" className="[&.active]:font-bold">
-					Settings
-				</Link>
-				<Link to="/dynamic" className="[&.active]:font-bold">
-					Dynamic
 				</Link>
 			</div>
 			<hr />
@@ -41,12 +30,4 @@ export const Route = createRootRoute({
 			<TanStackRouterDevtools />
 		</>
 	),
-	notFoundComponent: () => <div>404: Not Found</div>,
-	validateSearch: rootSearchSchema,
-	search: {
-		middlewares: [
-			stripSearchParams({ root: "a" }),
-			retainSearchParams(["root"]), // Known issue: https://github.com/TanStack/router/issues/2845
-		],
-	},
 });
--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -2,22 +2,22 @@ import { createFileRoute } from "@tanstack/react-router";
 import { z } from "zod";
 
 const aboutSearchSchema = z.object({
-	example: z.enum(["foo", "bar", "baz"]),
+	allowed: z.enum(["foo", "bar", "baz"]),
 });
 
 export const Route = createFileRoute(
-	"/(pathlessLayoutDemos)/_pathlessLayout/about",
+	"/about",
 )({
 	component: About,
 	validateSearch: aboutSearchSchema,
 });
 
 function About() {
-	const { example } = Route.useSearch();
+	const { allowed } = Route.useSearch();
 	return (
 		<div className="p-2">
 			Hello from About!
-			<p>Search Param: {example}</p>
+			<p>Search Param: {allowed}</p>
 		</div>
 	);
 }
