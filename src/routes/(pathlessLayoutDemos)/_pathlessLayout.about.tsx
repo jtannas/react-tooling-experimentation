e9e30829cee7b037ceb6901c10ecd522e550651b
Joel Tannas <jtannas@gmail.com>
2025-05-20 18:10:10 -0600

    demo usage of search parameter validation

--- src/routes/(pathlessLayoutDemos)/_pathlessLayout.about.tsx
+++ src/routes/(pathlessLayoutDemos)/_pathlessLayout.about.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
+
+const aboutSearchSchema = z.object({
+	example: z.enum(["foo", "bar", "baz"]).catch("foo"),
+});
 
 export const Route = createFileRoute(
 	"/(pathlessLayoutDemos)/_pathlessLayout/about",
 )({
 	component: About,
+	validateSearch: aboutSearchSchema,
 });
 
 function About() {
