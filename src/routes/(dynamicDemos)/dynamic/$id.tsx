1930f46bbc1652d1e2152760fbd3dad537ddfdab
Joel Tannas <jtannas@gmail.com>
2025-05-27 17:32:36 -0600

    leave notes on using search params in loaders

--- src/routes/(dynamicDemos)/dynamic/$id.tsx
+++ src/routes/(dynamicDemos)/dynamic/$id.tsx
@@ -5,6 +5,12 @@ const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const Route = createFileRoute("/(dynamicDemos)/dynamic/$id")({
 	component: RouteComponent,
+	// Note: search parameters are not _directly_ available to the loaders and associated tools
+	// This is to prevent putting data into the cache that is tied to a search param without that
+	// params being marked as relevant to the loaders.
+	// https://tanstack.com/router/latest/docs/framework/react/guide/data-loading#using-search-params-in-loaders
+	// It is possible to access them by defining them in the loaderDeps, and the accessing the deps in the loader.
+	// This ensures that important search params are explicitly marked as dependencies
 	beforeLoad: () => ({ injectedContext: "Powers" }),
 	loader: async ({ params, context }) => {
 		await delay(1000);
