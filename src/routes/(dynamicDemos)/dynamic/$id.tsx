bb5f754033d686972898378874da68a39510daa8
Joel Tannas <jtannas@gmail.com>
2025-05-27 17:11:25 -0600

    demo usage of route loaders

--- src/routes/(dynamicDemos)/dynamic/$id.tsx
+++ src/routes/(dynamicDemos)/dynamic/$id.tsx
@@ -1,22 +1,26 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
+import { CustomLink } from "../../../components/CustomLink";
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const Route = createFileRoute("/(dynamicDemos)/dynamic/$id")({
 	component: RouteComponent,
+	loader: async ({ params }) => {
+		await delay(1000);
+		return `loaded ${params.id}`;
+	},
 });
 
 function RouteComponent() {
 	const { id } = Route.useParams();
-
+	const loaded = Route.useLoaderData();
 	return (
 		<div>
 			Hello "/dynamic/{id}"!
-			<Link
-				className="block"
-				to="."
-				params={(prev) => ({ ...prev, id: id + 1 })}
-			>
-				Path param is a string by default
-			</Link>
+			<CustomLink to="." params={(prev) => ({ ...prev, id: id + 1 })}>
+				<p>Path param is a string by default</p>
+			</CustomLink>
+			<p>{loaded}</p>
 		</div>
 	);
 }
