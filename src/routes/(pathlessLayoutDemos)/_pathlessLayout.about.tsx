9d93d698b9d27b845bb9b80182ea0f0c16800b09
Joel Tannas <jtannas@gmail.com>
2025-06-03 17:32:02 -0600

    demo hook-based navigation blocking

--- src/routes/(pathlessLayoutDemos)/_pathlessLayout.about.tsx
+++ src/routes/(pathlessLayoutDemos)/_pathlessLayout.about.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link, useBlocker } from "@tanstack/react-router";
 import { z } from "zod";
 
 const aboutSearchSchema = z.object({
@@ -14,10 +14,35 @@ export const Route = createFileRoute(
 
 function About() {
 	const { example } = Route.useSearch();
+
+	const { proceed, status, reset } = useBlocker({
+		shouldBlockFn: ({ current, next }) => {
+			return current.fullPath === "/about" && next.fullPath === "/sample";
+		},
+		withResolver: true,
+	});
+
+	if (status === "blocked")
+		return (
+			<p>
+				You have been blocked!
+				<br />
+				<button type="button" className="block bg-gray-500" onClick={reset}>
+					Reset block
+				</button>
+				<button type="button" className="block bg-green-500" onClick={proceed}>
+					Proceed to /sample
+				</button>
+			</p>
+		);
+
 	return (
 		<div className="p-2">
 			Hello from About!
 			<p>Search Param: {example}</p>
+			<Link to="/sample" className="bg-red-500">
+				Blocked link to Sample
+			</Link>
 		</div>
 	);
 }
