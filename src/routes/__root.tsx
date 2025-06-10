e52ea2c277ba90e0940af94e6bbe1a1bb0ccbd8f
Joel Tannas <jtannas@gmail.com>
2025-06-10 15:57:33 -0600

    drop in new sidebar; format

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,9 +7,11 @@ import {
 	useUser,
 } from "@clerk/clerk-react";
 import type { QueryClient } from "@tanstack/react-query";
-import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import { createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import Header from "~/components/Header";
+import { AppSidebar } from "~/components/app-sidebar";
+import { SiteHeader } from "~/components/site-header";
+import { SidebarInset, SidebarProvider } from "~/components/ui/sidebar";
 import ClerkProvider from "~/integrations/clerk/provider.tsx";
 import { UserContext } from "~/integrations/clerk/useAppUser";
 import TanStackQueryLayout from "~/integrations/tanstack-query/layout.tsx";
@@ -44,9 +46,23 @@ function SignedInComponent() {
 
 	return (
 		<UserContext value={{ user }}>
-			<Header />
-			<div className="[view-transition-name:main-content]">
-				<Outlet />
+			<div className="[--header-height:calc(--spacing(14))]">
+				<SidebarProvider className="flex flex-col">
+					<SiteHeader />
+					<div className="flex flex-1">
+						<AppSidebar />
+						<SidebarInset>
+							<div className="flex flex-1 flex-col gap-4 p-4">
+								<div className="grid auto-rows-min gap-4 md:grid-cols-3">
+									<div className="bg-muted/50 aspect-video rounded-xl" />
+									<div className="bg-muted/50 aspect-video rounded-xl" />
+									<div className="bg-muted/50 aspect-video rounded-xl" />
+								</div>
+								<div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+							</div>
+						</SidebarInset>
+					</div>
+				</SidebarProvider>
 			</div>
 		</UserContext>
 	);
