a8190539558ac1c938cd0dc9a3195a364969c8a5
Joel Tannas <jtannas@gmail.com>
2025-06-11 12:21:06 -0600

    suppress linter error from known Biome issue

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -88,6 +88,7 @@ function SidebarProvider({
 	);
 
 	// Helper to toggle the sidebar.
+	// biome-ignore lint/correctness/useExhaustiveDependencies: https://github.com/biomejs/biome/issues/5914#issuecomment-2963696858
 	const toggleSidebar = React.useCallback(() => {
 		return isMobile ? setOpenMobile((open) => !open) : setOpen((open) => !open);
 	}, [isMobile, setOpen, setOpenMobile]);
@@ -112,6 +113,7 @@ function SidebarProvider({
 	// This makes it easier to style the sidebar with Tailwind classes.
 	const state = open ? "expanded" : "collapsed";
 
+	// biome-ignore lint/correctness/useExhaustiveDependencies: https://github.com/biomejs/biome/issues/5914#issuecomment-2963696858
 	const contextValue = React.useMemo<SidebarContextProps>(
 		() => ({
 			state,
