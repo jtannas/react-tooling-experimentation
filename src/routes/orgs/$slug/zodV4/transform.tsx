cfc887ab3fcddb6b58cac0aa885672a128db398d
Joel Tannas <jtannas@gmail.com>
2025-07-22 15:57:58 -0600

    demo usage of zod transforms for data pipelines

--- src/routes/orgs/$slug/zodV4/transform.tsx
+++ src/routes/orgs/$slug/zodV4/transform.tsx
@@ -1,4 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
+import z, { type ZodSafeParseResult } from "zod/v4";
+import { Input } from "~/components/ui/input";
+
+const idToUser = z
+	.number()
+	.gte(1)
+	.transform(async (id) => {
+		// fetch user from database
+		await new Promise((resolve) => setTimeout(resolve, 500));
+		return `User: ${id}`;
+	});
 
 export const Route = createFileRoute("/orgs/$slug/zodV4/transform")({
 	component: RouteComponent,
@@ -9,5 +21,39 @@ export const Route = createFileRoute("/orgs/$slug/zodV4/transform")({
 });
 
 function RouteComponent() {
-	return <div>Hello "/orgs/$slug/zodV4/transform"!</div>;
+	const [num, setNum] = useState(0);
+	const [user, setUser] = useState<ZodSafeParseResult<string> | undefined>();
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			const zUser = await idToUser.safeParseAsync(num);
+			setUser(zUser);
+		};
+		fetchUser();
+	}, [num]);
+
+	return (
+		<div>
+			<Input
+				placeholder="XP"
+				type="number"
+				value={num}
+				onChange={(e) => setNum(e.currentTarget.valueAsNumber)}
+			/>
+			<p>{user?.data}</p>
+			{user?.error && (
+				<p>
+					Errors:
+					<ul>
+						{z
+							.prettifyError(user.error)
+							.split(/\n/)
+							.map((error) => (
+								<li key={error}>{error}</li>
+							))}
+					</ul>
+				</p>
+			)}
+		</div>
+	);
 }
